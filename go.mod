e4654f5ade0ff2cd79805a842c1e5496ffbf2bd5
Rafal Gajdulewicz <rafal.gajdulewicz@sourcegraph.com>
2022-08-26 11:11:52 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/rafax/ibmreposizes
+
+go 1.18
+
+require (
+	github.com/google/go-github/v47 v47.0.0
+	github.com/inhies/go-bytesize v0.0.0-20220417184213-4913239db9cf
+	github.com/schollz/progressbar/v3 v3.9.0
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+)
+
+require (
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/google/go-github/v47/github"
+	"github.com/inhies/go-bytesize"
+	"github.com/schollz/progressbar/v3"
+	"golang.org/x/oauth2"
+)
+
+var pageSizeFlag = flag.Int("page-size", 100, "Size of page to request from GitHub API")
+var orgNameFlag = flag.String("org-name", "", "Size of page to request from GitHub API")
+var apiTokenFlag = flag.String("api-token", "", "Size of page to request from GitHub API")
+
+func main() {
+	flag.Parse()
+	if *orgNameFlag == "" {
+		log.Fatalln("org-name flag is required")
+	}
+	if *apiTokenFlag == "" {
+		log.Fatalln("api-token flag is required")
+	}
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: *apiTokenFlag},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+	bar := progressbar.Default(-1, "Fetching repos for org "+*orgNameFlag)
+	opt := &github.RepositoryListByOrgOptions{Type: "public", ListOptions: github.ListOptions{PerPage: *pageSizeFlag}}
+	var allRepos []*github.Repository
+	for {
+		repos, resp, err := client.Repositories.ListByOrg(ctx, *orgNameFlag, opt)
+		if err != nil {
+			log.Fatalf("fetching repos failed: %v", err)
+		}
+		bar.Add(len(repos))
+		allRepos = append(allRepos, repos...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+	sizeKB := 0
+	for _, r := range allRepos {
+		sizeKB += *r.Size
+	}
+	size, err := bytesize.Parse(fmt.Sprint(sizeKB, "KB"))
+	if err != nil {
+		log.Fatalf("calculating size failed: %v", err)
+	}
+	fmt.Println()
+	fmt.Printf("Found %d repos for org %s, %s total size\n", len(allRepos), *orgNameFlag, size)
+}
